3c26c958a91a87c191fb82a97c7650accd3f1e0a
Nguyen Hoan <nguyenvanhoan0902@gmail.com>
2023-07-05 10:31:35 +0700

    add new

--- app.py
+++ app.py
@@ -52,27 +52,151 @@ def decrypt_file(file_path, key, mode, iv=None):
     unpadded_data = unpad(decrypted_data, AES.block_size)
     return unpadded_data
 
+
+def search():
+    st.markdown("""
+            <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <style>
+                body {
+                font-family: serif0;
+                }
+
+                * {
+                box-sizing: border-box;
+                }
+
+                form.example input[type=text] {
+                padding: 10px;
+                font-size: 17px;
+                border: 1px solid grey;
+                float: left;
+                width: 80%;
+                background: #f1f1f1;
+                }
+
+                form.example button {
+                float: left;
+                width: 20%;
+                padding: 10px;
+                background: #545454;
+                color: white;
+                font-size: 17px;
+                border: 1px solid grey;
+                border-left: none;
+                cursor: pointer;
+                }
+
+                form.example button:hover {
+                background: #545454;
+                }
+
+                form.example::after {
+                content: "";
+                clear: both;
+                display: table;
+                }
+            </style>
+            </head>
+            <body>
+            """, 
+            unsafe_allow_html=True)
+
+    st.markdown("""
+            <form class="example" action="" style="margin:auto;max-width:300px">
+            <input type="text" placeholder="Search.." name="search2">
+            <button type="submit"><i class="fa fa-search"></i></button>
+            </form>""",
+            unsafe_allow_html=True)
+    st.markdown(
+                """
+                <head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+                </head>
+                """,
+                unsafe_allow_html=True
+            )
+
+
+def list_author():
+    st.markdown(
+                """
+                <style>
+                .c {
+                    margin-top: 100px ;
+                    }
+                </style>
+
+                <div class="c"></div>
+                """,
+                unsafe_allow_html=True
+            )
+    st.markdown(
+                """
+                <style>
+                .faded-text {
+                    color: rgba(0, 0, 0, 0.5);
+                    text-align: right;
+                }
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+
+    st.markdown('<h6 class="faded-text">Nhóm sinh viên:</h6>', unsafe_allow_html=True)
+    st.markdown(
+                """
+                <style>
+                .styled-metric {
+                    background-color: #f0f2f6;
+                    padding: 10px;
+                    border-radius: 5px;
+                    font-size: 18px;
+                    color: rgba(0, 0, 0, 0.5);
+                    display: flex;
+                    align-items: center;
+                }
+                .styled-metric i {
+                    margin-right: 10px;
+                    font-size: 20px;
+                }
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+
+            # Display the styled metrics with Facebook icons
+    st.markdown('<div class="styled-metric"><i class="far fa-user"></i> Nguyễn Văn Hoàn</div>', unsafe_allow_html=True)
+    st.markdown('<div class="styled-metric"><i class="far fa-user"></i>Nguyễn Đức Hiếu</div>', unsafe_allow_html=True)
+    st.markdown('<div class="styled-metric"><i class="far fa-user"></i>Nguyễn T.Huy Hoàng</div>', unsafe_allow_html=True)
+    st.markdown('<div class="styled-metric"><i class="far fa-user"></i> Vũ Nhật Minh</div>', unsafe_allow_html=True)
+    st.markdown('<div class="styled-metric"><i class="far fa-user"></i> Hoàng Văn Tú</div>', unsafe_allow_html=True)
+        
+        
 def main():
     # Streamlit app
     
     with st.sidebar:
+        image=Image.open("logo_app_c.png")
+        st.image(image=image)
         st.markdown("#### Chọn chức năng ####")
         selected = option_menu("Main Menu", ["Mã hóa", "Giải mã"],
                                icons=['lock', 'key'],
                                menu_icon="cast", default_index=0, styles={
                 "container": {"padding": "5!important", "background-color": "#fafafa"},
                 "icon": {"color": "black", "font-size": "15px"},
-                "nav-link": {"font-size": "15px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
+                "nav-link": {"font-size": "15px", "text-align": "left", "margin": "0px", "--hover-color": "f0f0f0","color": "#545454"},
             })
 
     # User input
     if selected == "Mã hóa":
         with col1:
             col1.title("Block Cipher Encryption")
-            uploaded_en_file = st.file_uploader("Upload a file", type=["txt", "enc"])
-            key_length = st.selectbox("Select key length:", [128, 192, 256])
-            mode = st.selectbox("Select encryption:", list(MODES.keys()))
-            key_input = st.text_input("Enter the encryption key (hex format):")
+            uploaded_en_file = st.file_uploader("Tải lên file để mã hóa", type=["txt", "enc"])
+            key_length = st.selectbox("Chọn độ dài khóa:", [128, 192, 256])
+            mode = st.selectbox("Chọn chế độ mã khối:", list(MODES.keys()))
+            key_input = st.text_input("Nhập khóa bí mật (hex format):")
 
             # Initialize key and IV
             key = None
@@ -80,19 +204,39 @@ def main():
 
             # Render IV input if required by the selected mode
             if mode in ["CBC", "CTR", "CFB", "OFB"]:
-                iv_input = st.text_input("Enter the IV (hex format):")
+                iv_input = st.text_input("Nhập giá trị IV (hex format):")
                 try:
                     iv = bytes.fromhex(iv_input) if iv_input else None
                 except ValueError:
-                    st.warning("Invalid IV format. Please enter a valid IV in hex format.")
+                    st.warning("Giá trị IV chưa đúng. Vui lòng nhập lại")
             if key_input:
                 try:
                     key = bytes.fromhex(key_input)
                     if len(key) * 8 != key_length:
-                        st.warning(f"Key length should be {key_length} bits.")
+                        st.warning(f"Giá trị khóa bí mật phải là {key_length} bits.")
                         key = None
                 except ValueError:
-                    st.warning("Invalid key format. Please enter a valid key in hex format.")
+                    st.warning("Giá trị khóa chưa đúng. Vui lòng nhập lại")
+            if mode =="ECB":
+                en_img = Image.open('ECB.png')
+                st.markdown("Sơ đồ mã hóa")
+                st.image(en_img)
+            if mode =="CBC":
+                en_img = Image.open('CBC.png')
+                st.markdown("Sơ đồ mã hóa")
+                st.image(en_img)
+            if mode =="CFB":
+                en_img = Image.open('CFB.png')
+                st.markdown("Sơ đồ mã hóa")
+                st.image(en_img)
+            if mode =="OFB":
+                en_img = Image.open('OFB.png')
+                st.markdown("Sơ đồ mã hóa")
+                st.image(en_img)
+            if mode =="CTR":
+                en_img = Image.open('CTR.png')
+                st.markdown("Sơ đồ mã hóa")
+                st.image(en_img)
 
             # Encrypt
             # Encrypt button
@@ -112,12 +256,12 @@ def main():
 
                         # Display encryption time
                         encryption_time = end_time - start_time
-                        st.info(f"Encryption time: {encryption_time:.4f} seconds")
+                        st.info(f"Thời gian mã hóa: {encryption_time:.4f} giây")
 
                         encrypted_file = BytesIO(encrypted_data)
 
-                        st.success("Encryption successful!")
-                        st.text("Encrypted file created.")
+                        st.success("Mã hóa thành công!")
+                        st.info("Đã tạo file mã hóa.Bấm để tải xuống.")
                         st.download_button(
                             label="Download Encrypted File",
                             data=encrypted_file,
@@ -128,94 +272,26 @@ def main():
                         # Clean up the temporary directory
                         temp_dir.cleanup()
                     except Exception as e:
-                        st.error(f"Encryption error: {str(e)}")
+                        st.error(f"Mã hóa thất bại: {str(e)}")
                 else:
-                    st.warning("Please upload a file and enter a valid encryption key.")
+                    st.warning("Hãy tải file và nhập khóa !!!")
         with col2:
-            image = Image.open("logo_app_c.png")
-            st.image(image=image)
-            st.markdown(
-                """
-                <head>
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-                </head>
-                <style>
-                    .a {
-                        background-color: #f0f2f6;
-                        padding: 20px;
-                        text-align: center;
-                    }
-                    
-                    .icon-list {
-                        display: flex;
-                        flex-direction: column;
-                        justify-content: center;
-                        align-items: center;
-                        margin-top: 10px;
-                    }
+            
+            search()
+            list_author()
+            
+            
 
-                    .icon-list-item {
-                        margin: 10px;
-                        text-align: center;
-                        cursor: pointer;
-                    }
 
-                    .icon-list-item i {
-                        font-size: 20px;
-                        color: black;
-                    }
-                    .icon-list-item span {
-                    color: black;
-                    text-decoration: none;
-                    font-weight: bold;
-                    }
-                </style>
-                
-                """,
-                unsafe_allow_html=True,
-            )
-
-            st.markdown(
-                """
-                <div class="a">
-                    <h6>Liên hệ với chúng tôi</h6>
-                    <div class="icon-list">
-                        <div class="icon-list-item">
-                            <a href="https://github.com" target="_blank">
-                                <i class="fab fa-github"></i>
-                                <span> Github</span>
-                            </a>
-                        </div>
-                        <div class="icon-list-item">
-                            <a href="https://twitter.com" target="_blank">
-                                <i class="fab fa-twitter"></i>
-                                <span> Twitter</span>
-                            </a>
-                        </div>
-                        <div class="icon-list-item">
-                            <a href="https://youtube.com" target="_blank">
-                                <i class="fab fa-youtube"></i>
-                                <span> Youtube</span>
-                            </a>
-                        </div>
-                        <div class="icon-list-item">
-                            <a href="https://facebook.com" target="_blank">
-                                <i class="fab fa-facebook"></i>
-                                <span> Facebook</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+            
+            
     elif selected == "Giải mã":
         col1.title("Block Cipher Decryption")
         with col1:
-            uploaded_de_file = st.file_uploader("Upload a file to decryp", type=["txt", "enc"])
-            key_length = st.selectbox("Select key length:", [128, 192, 256])
-            mode = st.selectbox("Select decryption mode:", list(MODES.keys()))
-            key_input = st.text_input("Enter the encryption key (hex format):")
+            uploaded_de_file = st.file_uploader("Tải file lên để giải mã", type=["txt", "enc"])
+            key_length = st.selectbox("Chọn độ dài khóa :", [128, 192, 256])
+            mode = st.selectbox("Chọn chế độ :", list(MODES.keys()))
+            key_input = st.text_input("Nhập khóa bí mật :")
 
             # Initialize key and IV
             key = None
@@ -223,21 +299,21 @@ def main():
 
             # Render IV input if required by the selected mode
             if mode in ["CBC", "CTR", "CFB", "OFB"]:
-                iv_input = st.text_input("Enter the IV (hex format):")
+                iv_input = st.text_input("Nhập giá trị IV (hex format):")
                 try:
                     iv = bytes.fromhex(iv_input) if iv_input else None
                 except ValueError:
-                    st.warning("Invalid IV format. Please enter a valid IV in hex format.")
+                    st.warning("Giá trị IV chưa đúng. Vui lòng nhập lại")
 
             # Validate and convert the key
             if key_input:
                 try:
                     key = bytes.fromhex(key_input)
                     if len(key) * 8 != key_length:
-                        st.warning(f"Key length should be {key_length} bits.")
+                        st.warning(f"Khóa phải dài {key_length} bits.")
                         key = None
                 except ValueError:
-                    st.warning("Invalid key format. Please enter a valid key in hex format.")
+                    st.warning("Giá trị khóa chưa đúng. Vui lòng nhập lại")
 
             # Decrypt button
             if st.button("Decrypt"):
@@ -256,12 +332,12 @@ def main():
 
                         # Display decryption time
                         decryption_time = end_time - start_time
-                        st.info(f"Decryption time: {decryption_time:.4f} seconds")
+                        st.info(f"Thời gian giải mã: {decryption_time:.4f} giây")
 
                         decrypted_file = BytesIO(decrypted_data)
 
-                        st.success("Decryption successful!")
-                        st.text("Decrypted file created.")
+                        st.success("Giải mã thành công!")
+                        st.info("Đã tạo file giải mã. Bấm để tải")
                         st.download_button(
                             label="Download Decrypted File",
                             data=decrypted_file,
@@ -272,9 +348,13 @@ def main():
                         # Clean up the temporary directory
                         temp_dir.cleanup()
                     except Exception as e:
-                        st.error(f"Decryption error: {str(e)}")
+                        st.error(f"Giải mã thất bại: {str(e)}")
                 else:
-                    st.warning("Please upload a file and enter a valid decryption key.")
+                    st.warning("Vui lòng tải lên file và nhập khóa")
+
+        with col2:
+            search()
+            list_author()
 
 if __name__ == "__main__":
     main()
